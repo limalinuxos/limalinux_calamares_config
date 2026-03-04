b3a6ba46582dc98c4fa765bf41dcf208e7ba8826
limalinuxos <limalinuxos@proton.me>
2026-03-05 00:46:05 +0100

    update

---
+++ up.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -eo pipefail
+##################################################################################################################
+# Author    : Daiel Duarte
+# Github    : https://github.com/danielduarte86
+# Github    : https://github.com/limalinuxos
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# variables and functions
+workdir=$(pwd)
+dir="calamares-3.3.14.r87.g3f6cd83-1"
+source="/home/daniel/limalinuxos/limalinux_pkgbuild/"
+destiny="/home/daniel/limalinuxos/limalinux_calamares_config/etc/calamares/pkgbuild/"
+
+##################################################################################################################
+
+if [ -d "$destiny" ]; then
+    rm -rf "$destiny"
+fi
+
+if ! [ -d "$destiny" ]; then
+    mkdir "$destiny"
+fi
+
+cp -rv $source$dir/* $destiny
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "update"
+
+# Push the local files to github
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+git push -u origin "$branch"
+
+echo
+tput setaf 6
+echo "##############################################################"
+echo "###################  $(basename $0) done"
+echo "##############################################################"
+tput sgr0
+echo
